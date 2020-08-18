86bcb12b1e4cb493e6f8d083bf74cbf71403c3a0
vuru <sueleyman.vurucu@amova.eu>
2020-08-18 12:52:53 +0200

    inital

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule,Inject } from '@angular/core';
+import { Routes, Router, RouterModule } from '@angular/router'
+import { ApmService } from '@elastic/apm-rum-angular'
 
 import { AppComponent } from './app.component';
+
+const routes: Routes = [
+
+]
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [
+  {
+    provide: ApmService,
+    useClass: ApmService,
+    deps: [Router]
+  }
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+  constructor(@Inject(ApmService) service: ApmService) {
+    // API is exposed through this apm instance
+    const apm = service.init({
+      serviceName: 'angular-app',
+      serverUrl: 'http://localhost:8200'
+    })
+
+    apm.setUserContext({
+      'username': 'foo',
+      'id': 'bar'
+    })
+  }
+	}
